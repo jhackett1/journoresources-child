14c744bd562df4eca3f1b2465ab091c3f10342b2
Joshua Hackett <hello@joshuahackett.com>
2018-09-08 23:16:10 +0100

    Added responsive meta tag

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <html <?php language_attributes(); ?>>
     <!--<![endif]-->
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
